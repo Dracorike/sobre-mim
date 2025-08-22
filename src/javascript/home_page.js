827545bfb21b909f77e0d49e903be6d2e680ac9b
Dracorike <pedrohenriqueknd@gmail.com>
2025-08-21 22:52:44 -0300

    Abstrair o caminho para as micro páginas e colocar em uma variável

--- src/javascript/home_page.js
+++ src/javascript/home_page.js
@@ -1,19 +1,21 @@
+const MICRO_PAGES_PATH = '../../res/html/micro_pages'
+
 function clickOnAboutMe() { 
-    document.getElementById("page_content").data = '../../res/html/micro_pages/about_me.html'
+    document.getElementById("page_content").data = `${MICRO_PAGES_PATH}/about_me.html` 
 }
 
 function clickOnPortifolio() { 
-    document.getElementById("page_content").data = '../../res/html/micro_pages/portfolio.html'
+    document.getElementById("page_content").data = `${MICRO_PAGES_PATH}/portfolio.html`
 }
 
 function clickOnAcademicBackground() { 
-    document.getElementById("page_content").data = '../../res/html/micro_pages/academic_background.html'
+    document.getElementById("page_content").data = `${MICRO_PAGES_PATH}/academic_background.html`
 }
 
 function clickOnCareerPage() { 
-    document.getElementById("page_content").data = '../../res/html/micro_pages/career_page.html'
+    document.getElementById("page_content").data = `${MICRO_PAGES_PATH}/career_page.html`
 }
 
 function clickOnContactsLinks() { 
-    document.getElementById("page_content").data = '../../res/html/micro_pages/contact_links.html'
+    document.getElementById("page_content").data = `${MICRO_PAGES_PATH}/contact_links.html`
 }
