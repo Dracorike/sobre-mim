13667ec1e092f1e790a5ed05124bfe948784bc9f
Dracorike <pedrohenriqueknd@gmail.com>
2025-08-21 22:45:50 -0300

    Muda de página está funcionando

---
+++ src/javascript/home_page.js
@@ -0,0 +1,19 @@
+function clickOnAboutMe() { 
+    document.getElementById("page_content").data = '../../res/html/micro_pages/about_me.html'
+}
+
+function clickOnPortifolio() { 
+    document.getElementById("page_content").data = '../../res/html/micro_pages/portfolio.html'
+}
+
+function clickOnAcademicBackground() { 
+    document.getElementById("page_content").data = '../../res/html/micro_pages/academic_background.html'
+}
+
+function clickOnCareerPage() { 
+    document.getElementById("page_content").data = '../../res/html/micro_pages/career_page.html'
+}
+
+function clickOnContactsLinks() { 
+    document.getElementById("page_content").data = '../../res/html/micro_pages/contact_links.html'
+}
